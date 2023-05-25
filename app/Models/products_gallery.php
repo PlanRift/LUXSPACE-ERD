3716294629bc0157f00020f185c704cb89a1a043
PlanRift <dzikramufidatovid@gmail.com>
2023-05-25 11:37:36 +0700

    added a new index page

--- app/Models/products_gallery.php
+++ app/Models/products_gallery.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class products_gallery extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'product_id', 'url', 'is_featured'
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+    public function product(): HasOne
+    {
+        return $this->hasOne(Products::class, 'id', 'product_id');
+    }
 }
--- app/Models/transactionItems.php
+++ app/Models/transactionItems.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class transactionItems extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $guarded = [];
+
+    public function product(): HasOne
+    {
+        return $this->hasOne(Products::class, 'id', 'product_id');
+    }
 }
